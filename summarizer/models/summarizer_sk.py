d63d90671d783d3154cbed67afa14914877ba74a
manu-spherical <manuhegdev@gmail.com>
2019-05-09 09:28:31 +0530

    changes

--- summarizer/models/summarizer_sk.py
+++ summarizer/models/summarizer_sk.py
@@ -2,17 +2,46 @@ import numpy as np
 from nltk.tokenize import sent_tokenize
 from skipthoughts import skipthoughts
 from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
 from sklearn.metrics import pairwise_distances_argmin_min
+from attn import load_ed,eval_ed
 import os
 import sys
 sys.path.append('skipthoughts')
 
+def reduction_scheme_len(doc_len,scheme='half'):
+  schemes = {'half':lambda n:int(np.ceil(float(n)*0.5)),
+             'onethird':lambda n:int(np.ceil(float(n)/3)),'onefourth':lambda n:int(np.ceil(float(n)/4)),
+             'sqrt':lambda n:int(np.ceil(float(n)**0.5))}
+  return schemes.get(scheme)(doc_len)
+
+def getval(dfg,i,index=True):
+    if index:
+        return dfg.loc[i,:][1].index.tolist()
+    return list(dfg.loc[i,:][1].loc[:,'doc'])
+
+def get_clusters(doc,rsi=0,index=True):#rsi - reduction sceheme index
+    reduction_schemes = ['half','onethird','onefourth','sqrt',]
+    random_state = 0
+    n_clusters = max([2,reduction_scheme_len(len(doc),reduction_schemes[rsi])])
+    print('Number of clusters =',n_clusters)
+    km = KMeans(n_clusters=n_clusters,random_state=random_state)
+    kmc = km.fit(doc)
+    dc = [e.tolist() for e in doc ]
+    df = pd.DataFrame({'doc':dc,'cluster':kmc.predict(doc)})
+    dfg = pd.DataFrame(df.groupby(by=['cluster']))
+    clusters = list(map( lambda i:getval(dfg,i,index),list(range(len(dfg)))))
+    return clusters
 
 def load_model():
     print('Loading pre-trained model...')
     global model, encoder
     model = skipthoughts.load_model()
     encoder = skipthoughts.Encoder(model)
+    try:
+        load_ed()
+    except Exception as e:
+        pass
     print('Encoding sentences...')
 
 
@@ -58,9 +87,19 @@ def skipthought_encode(docs):
         enc_docs[i] = enc_sentences[begin:end]
     return enc_docs
 
+def dec_sum(enc_doc,doc):
+    sdoc = split_sentences([doc])[0]
+    clusters_alt = get_clusters(enc_doc,rsi=0,index=True)
+    sent_clusters = list(map(lambda cluster: list(map(lambda index: sdoc[index],cluster)),clusters_alt))
+    dec_summ = []
+    for s in sent_clusters:
+        dec_summ.append(eval_ed(s))
+    return dec_summ
+
 
 def summarize(docs):
     n_docs = len(docs)
+    summary_ed =[]
     summary = [None]*n_docs
     print('Preprecesing...')
     preprocess(docs)
@@ -69,9 +108,14 @@ def summarize(docs):
     print('Starting to encode...')
     enc_docs = skipthought_encode(docs)
     print('Encoding Finished')
+    tsne_components = 512
     for i in range(n_docs):
         enc_doc = enc_docs[i]
-        n_clusters = int(np.ceil(len(enc_doc)**0.5))
+        enc_doc = TSNE(n_components=tsne_components).fit_transform(enc_doc)
+        n_clusters = int(reduction_scheme_len(len(docs[i])))
+        n_clusters = max([1,reduction_scheme_len(len(docs[i]),'sqrt')])
+        
+        
         kmeans = KMeans(n_clusters=n_clusters, random_state=0)
         kmeans = kmeans.fit(enc_doc)
         avg = []
@@ -83,5 +127,8 @@ def summarize(docs):
             kmeans.cluster_centers_, enc_doc)
         ordering = sorted(range(n_clusters), key=lambda k: avg[k])
         summary[i] = ' '.join([docs[i][closest[idx]] for idx in ordering])
+
+        summary_ed.append(dec_sum(enc_docs[i],docs[i]))
     print('Done')
+    
     return summary
